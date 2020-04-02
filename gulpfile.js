ec43da0177095e2a8a2b22797101c157c8a5ba6f
BSFishy <mattprovost6@gmail.com>
2020-04-02 17:48:28 -0500

    Add routing path

--- gulpfile.js
+++ gulpfile.js
@@ -201,7 +201,7 @@ function deepCheck(projects, project) {
     return false;
 }
 
-let projects = [['component', 'routing'], ['component-parser', 'component-renderer', 'page'], ['module']];
+let projects = [['component', 'routing-path'], ['component-parser', 'component-renderer', 'page', 'routing'], ['module']];
 for (let project of fs.readdirSync('packages')) {
     if (fs.statSync(path.join('packages', project)).isDirectory() && fs.existsSync(path.join('packages', project, 'tsconfig.json'))) {
         if (!deepCheck(projects, project))
