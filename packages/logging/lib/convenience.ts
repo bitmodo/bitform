c6e7528111e6c453033281b9fb04c7d696f26161
BSFishy <mattprovost6@gmail.com>
2020-04-21 15:33:23 -0500

    Add root logger function

--- packages/logging/lib/convenience.ts
+++ packages/logging/lib/convenience.ts
@@ -1,5 +1,15 @@
-import { LoggerConfig, Logger }      from './logger';
+import { LoggerConfig, Logger } from './logger';
 
 export function logger(config?: LoggerConfig): Logger {
     return new Logger(config);
 }
+
+export let root: (config?: LoggerConfig) => Logger = (config?: LoggerConfig) => {
+    const log = logger(config);
+
+    root = () => {
+        return log;
+    };
+
+    return log;
+};
